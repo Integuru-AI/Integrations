6222c12ea23a4df5df5d7fdaa779ad91fa79095e
alanalanlu <alanlu1999@gmail.com>
2024-10-21 20:34:11 -0700

    Add robinhood

---
+++ main.py
@@ -0,0 +1,10 @@
+import asyncio
+from robinhood.robinhood_integration import RobinhoodIntegration
+
+async def main():
+    pass
+    # robinhood = RobinhoodIntegration("YOUR_ACCESS_TOKEN")
+    # await robinhood.get_tax_documents()
+
+if __name__ == "__main__":
+    asyncio.run(main())
